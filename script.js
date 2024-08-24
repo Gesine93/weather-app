a3c2360e787af8fd93241fbc3b9d2eb2230a8742
Gesine Fengler <gesine.fengler@gmx.de>
2024-08-24 23:56:15 +0200

    Added data to website Added a function that adds the relevant info to the HTML

--- script.js
+++ script.js
@@ -10,20 +10,37 @@ async function getWeather(location) {
 }
 
 function populateHtml(data) {
-    
+    if (data && data.days && data.days.length > 0) {
+        const todayText = document.querySelector("#text");
+        const maxtemptoday = document.querySelector(".today > .left > #maxtemp");
+        const mintemptoday = document.querySelector(".today > .left > #mintemp");
+        const iconnow = document.querySelector("#iconnow");
+        const timenow = document.querySelector(".today > .right > #time");
+        const tempnow = document.querySelector(".today > .right > #temp");
+
+        todayText.textContent = data.days[0].description;
+        maxtemptoday.textContent = data.days[0].tempmax;
+        mintemptoday.textContent = data.days[0].tempmin;
+        timenow.textContent = `${String(data.days[0].datetimeEpoch).slice(0,2)}:${String(data.days[0].datetimeEpoch).slice(2,4)}`;
+        tempnow.textContent = data.days[0].temp;
+    };
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.querySelector(".submit");
-    submitButton.addEventListener("click", (e) => {
+    submitButton.addEventListener("click", async (e) => {
         e.preventDefault();
-        let location = document.querySelector('input[name="title"]').value;
+        let location = document.querySelector('input[name="location"]').value;
         if (!location) {
             return; // Exit the function to prevent submission
         }   
-        let data = getWeather(location); 
+        const data = await getWeather(location); 
         if (data != "Error") {
             populateHtml(data);
+        } else {
+            const todayText = document.querySelector("#text").textContent;
+            todayText = "Error!";
         }
     })
 })
+
