af12286253c18dae40253f52be8e9775ea3c4c89
Gesine Fengler <gesine.fengler@gmx.de>
2024-08-26 15:56:27 +0200

    Added icons Added a function to the script file that adds an icon to the page depending on the icon attribute of the API data Added all necessary icons to the icon folder

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather(location) {
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=7HLBA6BCS4PDD2RNMW2FY68XV`);
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?iconSet=icons1&key=7HLBA6BCS4PDD2RNMW2FY68XV`);
         const data = await response.json();
         return data;
     }
@@ -23,7 +23,41 @@ function populateHtml(data) {
         mintemptoday.textContent = data.days[0].tempmin;
         timenow.textContent = `${String(data.days[0].datetimeEpoch).slice(0,2)}:${String(data.days[0].datetimeEpoch).slice(2,4)}`;
         tempnow.textContent = data.days[0].temp;
+        iconnow.src = getIcon(data.days[0].icon);
     };
+}
+
+function getIcon(data) {
+    const clear_day = "./icons/clear-day.svg";
+    const clear_night = "./icons/clear-night.svg";
+    const partly_cloudy_day = "./icons/partly-cloudy-day.svg";
+    const partly_cloudy_night = "./icons/partly-cloudy-night.svg";
+    const rain = "./icons/rain.svg";
+    const snow = "./icons/snow.svg";
+    const wind = "./icons/wind.svg";
+    const fog = "./icons/fog.svg";
+    const cloudy = "./icons/cloudy.svg";
+    if (data === "clear-day") {
+        return clear_day;
+    } else if (data === "clear-night") {
+        return clear_night;
+    } else if (data === "partly-cloudy-day") {
+        return partly_cloudy_day;
+    } else if (data === "partly-cloudy-night") {
+        return partly_cloudy_night;
+    } else if (data === "rain") {
+        return rain;
+    } else if (data === "snow") {
+        return snow;
+    } else if (data === "wind") {
+        return wind;
+    } else if (data === "fog") {
+        return fog;
+    } else if (data === "cloudy") {
+        return cloudy;
+    } else {
+        return "";
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
